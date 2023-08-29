5d61c1e408a8bba08e9119162d0856a2293ae276
GAGAN MAKHIJA <87359955+gaganmakhija@users.noreply.github.com>
2023-08-29 12:20:28 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Array Subset of another array/array-subset-of-another-array.java
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t-->0)
+        {
+            StringTokenizer stt = new StringTokenizer(br.readLine());
+            
+            long n = Long.parseLong(stt.nextToken());
+            long m = Long.parseLong(stt.nextToken());
+            long a1[] = new long[(int)(n)];
+            long a2[] = new long[(int)(m)];
+            
+            
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a1[i] = Long.parseLong(inputLine[i]);
+            }
+            String inputLine1[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < m; i++) {
+                a2[i] = Long.parseLong(inputLine1[i]);
+            }
+            
+            
+            Compute obj = new Compute();
+            System.out.println(obj.isSubset( a1, a2, n, m));
+            
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Compute {
+    public String isSubset( long a1[], long a2[], long n, long m) {
+        String res = "Yes";
+        
+        HashMap<Long, Integer> hm = new HashMap<Long, Integer>();
+        for(int i=0;i<n;i++){
+            if(hm.containsKey(a1[i])){
+                hm.put(a1[i], hm.get(a1[i])+1);
+            }
+            else{
+                hm.put(a1[i], 1);
+            }
+        }
+        
+        for(int j=0;j<m;j++){
+            if(hm.containsKey(a2[j]) && hm.get(a2[j])>0){
+                hm.put(a2[j], hm.get(a2[j])-1);
+            }
+            else{
+                res="No";
+            }
+        }
+        
+        return res;
+        
+    }
+}
