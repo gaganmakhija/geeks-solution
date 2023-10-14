73de2c557b4eef1875949c5410181073fdf72134
GAGAN MAKHIJA <87359955+gaganmakhija@users.noreply.github.com>
2023-10-14 12:39:57 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Add Minimum Characters/add-minimum-characters.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+class Solution
+{
+	public static int addMinChar(String str){
+		//code here
+		 int res = 0,l=0,r=str.length()-1;
+        while(l <= r){
+            if(str.charAt(l++) != str.charAt(r--)){
+                res++;
+                l=0;
+                r = str.length() - 1 - res;
+            }
+        }
+        return res;
+	}
+}
+
+//{ Driver Code Starts.
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+        	String str = read.readLine();
+            Solution ob = new Solution();
+            System.out.println(ob.addMinChar(str));
+        }
+        
+    }
+}
+
+// } Driver Code Ends
