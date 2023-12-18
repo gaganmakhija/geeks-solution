9fc059010c551b8b65d258f2e862b5c6837cd575
GAGAN MAKHIJA <87359955+gaganmakhija@users.noreply.github.com>
2023-12-18 20:28:55 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Rotating an Array/rotating-an-array.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            int d = Integer.parseInt(br.readLine().trim());
+
+            new Solution().leftRotate(arr, n, d);
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    void leftRotate(int[] arr, int n, int d) {
+        // code here
+        
+         int[] arr1=new int[n];
+        for(int i=0;i<n;i++){
+            int index=(i+d)%n;
+            arr1[i]=arr[index];
+        }
+        for(int i=0;i<n;i++){
+            arr[i]=arr1[i];
+        }
+    }
+}
